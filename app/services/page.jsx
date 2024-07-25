bab78e5595c46bd76d06185c038b012e226b7b1b
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-07-25 23:27:52 +0600

    service and resume page done

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -3,6 +3,7 @@
 import { Description } from "@radix-ui/react-dialog";
 import { BsArrowDownRight } from "react-icons/bs";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const services = [
   {
@@ -45,13 +46,34 @@ const Services = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
           }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
         >
           {services.map((service, index) => {
             return (
-              <div key={index}>
-                <div>
-                  <div>{service.num}</div>
+              <div
+                key={index}
+                className="flex-1 flex flex-col justify-center gap-6 group"
+              >
+                {/* top */}
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                    {service.num}
+                  </div>
+                  <Link
+                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                    href={service.href}
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl " />
+                  </Link>
                 </div>
+                {/* title */}
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {service.title}
+                </h2>
+                {/* description */}
+                <p className="text-white/60">{service.Description}</p>
+                {/* border */}
+                <div className="border-b border-white/20"></div>
               </div>
             );
           })}
